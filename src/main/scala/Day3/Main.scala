b1b6f62d8d4d4b7a28ad424e196a11f406b0e7a9
Barry van Veen <barry+github@barryvanveen.nl>
2023-11-10 12:23:20 +0100

    Day 3, part 2

--- src/main/scala/Day3/Main.scala
+++ src/main/scala/Day3/Main.scala
@@ -3,30 +3,29 @@ package Day3
 import input.InputFile
 import input.Util.readLines
 
+import scala.collection.AbstractSeq
+
 object Main extends InputFile {
   def main(args: Array[String]): Unit = {
     val input = readLines(getInputFile(args))
 
     val value1 = part1(input)
     println(s"Part 1: $value1")
-//    val value2 = part2(input)
-//    println(s"Part 2: $value2")
+    val value2 = part2(input)
+    println(s"Part 2: $value2")
   }
 
-  def part1(input: List[String]): Option[Int] = {
+  def part1(input: List[String]): Int = {
     val booleans = input.map(stringToIntList).transpose
 
     val length = booleans.last.length
-    if (booleans.exists(_.length != length))
-      return None
-
     val ones = booleans.map(_.filter(_ == 1).sum)
 
     val majority = math.ceil(length.toDouble / 2.0)
     val gammaRate = ones.map((a: Int) => booleanToInt(a > majority))
     val epsilonRate = gammaRate.map((a: Int) => booleanToInt(!(a == 1)))
 
-    Some(booleanListToBinaryValue(gammaRate) * booleanListToBinaryValue(epsilonRate))
+    booleanListToBinaryValue(gammaRate) * booleanListToBinaryValue(epsilonRate)
   }
 
   def stringToIntList(in: String): List[Int] = {
@@ -35,7 +34,7 @@ object Main extends InputFile {
     in.toCharArray.map(_.toInt - '0').toList
   }
 
-  def booleanListToBinaryValue(in: List[Int]): Int = {
+  def booleanListToBinaryValue(in: AbstractSeq[Int]): Int = {
     // go from right to left
     // multiply each number (0 or 1) with it's 2-power
     // sum all parts
@@ -50,7 +49,46 @@ object Main extends InputFile {
     !in
   }
 
-//  def part2(input: List[String]): Int = {
-//
-//  }
+  def stringToIntVector(in: String): Vector[Int] = {
+    // convert to individual characters
+    // then substract '0' from the char value to get correct integer values
+    in.toCharArray.map(_.toInt - '0').toVector
+  }
+
+  def part2(input: List[String]): Int = {
+    val booleans = input.map(stringToIntVector).toVector
+    val wordLength = booleans.apply(0).length
+
+    var oxygenValues = booleans;
+    var co2Values = booleans;
+
+    for i <- 0 to wordLength
+      if oxygenValues.size > 1 || co2Values.size > 1
+    do
+      oxygenValues = filterOxygenValues(oxygenValues, i)
+      co2Values = filterCo2Values(co2Values, i)
+
+    booleanListToBinaryValue(oxygenValues.apply(0)) * booleanListToBinaryValue(co2Values.apply(0))
+  }
+
+  def onesInBit(in: Vector[Vector[Int]], bit: Int): Int = {
+    // transpose to get rows, take first row and sum all the 1's
+    in.transpose.slice(bit, bit+1).flatten.sum
+  }
+
+  def filterOxygenValues(in: Vector[Vector[Int]], bit: Int): Vector[Vector[Int]] = {
+    if (in.size == 1)
+      return in
+
+    val majority = booleanToInt(onesInBit(in, bit).toDouble >= in.size / 2.toDouble)
+    in.filter((a: Vector[Int]) => a.apply(bit) == majority)
+  }
+
+  def filterCo2Values(in: Vector[Vector[Int]], bit: Int): Vector[Vector[Int]] = {
+    if (in.size == 1)
+      return in
+
+    val minority = booleanToInt(onesInBit(in, bit).toDouble < in.size / 2.toDouble)
+    in.filter((a: Vector[Int]) => a.apply(bit) == minority)
+  }
 }
--- src/test/scala/Day3/Day3Test.scala
+++ src/test/scala/Day3/Day3Test.scala
@@ -50,9 +50,45 @@ class Day3Test extends AnyFunSpec  {
         "01010",
       )
 
-      Day3.Main.part1(input) match
-        case Some(i) => assert(i == 198)
-        case None => throw AssertionError("Part 1 did not return an integer")
+      assert(Day3.Main.part1(input) == 198)
+    }
+  }
+
+  describe("Part 2") {
+    val onesInFirstBitCases =
+      Table(
+        ("input", "bit", "output"), // First tuple defines column names
+        (Vector(Vector(0, 0, 0), Vector(0, 0, 0), Vector(0, 0, 1)), 0, 0),
+        (Vector(Vector(1, 0, 0), Vector(0, 0, 0), Vector(0, 0, 1)), 0, 1),
+        (Vector(Vector(1, 0, 0), Vector(0, 0, 0), Vector(0, 0, 1)), 1, 0),
+        (Vector(Vector(1, 0, 0), Vector(0, 0, 0), Vector(0, 0, 1)), 2, 1),
+        (Vector(Vector(1, 0, 0), Vector(1, 0, 0), Vector(0, 0, 1)), 0, 2),
+        (Vector(Vector(1, 0, 0), Vector(1, 0, 0), Vector(1, 1, 1)), 0, 3),
+      )
+
+    it("it counts all the ones in the first bit") {
+      forAll(onesInFirstBitCases) { (input: Vector[Vector[Int]], bit: Int, output: Int) =>
+        assert(Day3.Main.onesInBit(input, bit) == output)
+      }
+    }
+
+    it("finds the correct life support rating") {
+      val input = List(
+        "00100",
+        "11110",
+        "10110",
+        "10111",
+        "10101",
+        "01111",
+        "00111",
+        "11100",
+        "10000",
+        "11001",
+        "00010",
+        "01010",
+      )
+
+      assert(Day3.Main.part2(input) == 230)
     }
   }
 }
